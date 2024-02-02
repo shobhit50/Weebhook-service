858aabd159132208fdf4e52236f26e6be2ac6f54
Shobhit  Parashar <91795621+shobhit50@users.noreply.github.com>
2024-02-02 12:10:56 +0530

    Update wsgi.py

--- Webhook/Webhook/wsgi.py
+++ Webhook/Webhook/wsgi.py
@@ -8,10 +8,9 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
-from django.conf import settings
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Webhook.Webhook.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Webhook.settings')
 
 application = get_wsgi_application()
 
